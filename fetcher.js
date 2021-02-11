1fa5dd73a455204aab249a1d467215a9d2b9c54b
Judit Mendez Puig <mendez.puig@gmail.com>
2021-02-11 05:12:07 +0000

    working fetcher.js

---
+++ fetcher.js
@@ -0,0 +1,14 @@
+let args = process.argv.slice(2);
+const fs = require('fs');
+const request = require('request');
+
+request(args[0], function (error, response, body) {
+  fs.writeFile(args[1], body, function (err) {
+    if (err) throw err;
+  });
+  console.log('Downloaded and saved ' + body.length + ' bytes to ' + args[1]);
+});
+
+
+// > node fetcher.js http://www.example.edu/ ./index.html
+// Downloaded and saved 3261 bytes to ./index.html
